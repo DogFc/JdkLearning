67081e8b0fc779ea247c48c0e945c998a8672a8e
chengcheng.feng <chengcheng.feng@weimob.com>
2017-09-12 10:39:09 +0800

    first commit

---
+++ src/main/java/com/cc/learning/Mian.java
@@ -0,0 +1,11 @@
+package com.cc.learning;
+
+/**
+ * @author chengcheng.feng. /  2017/9/9 18:10
+ */
+public class Mian {
+    public static void main(String[] args) {
+        System.out.println("Start JdkLearning");
+    }
+}
+
